6a0d5347d62a3a79b96062029458003fa4ad82c5
Stavros Korokithakis <stavros@korokithakis.net>
2013-10-31 00:32:14 +0200

    Fix typo.

--- encbup/encbup.py
+++ encbup/encbup.py
@@ -8,7 +8,7 @@ Options:
   -h --help                        Show this screen.
   -s --salt=<salt>                 The salt used for the key.
   -p --passphrase=<passphrase>     The encryption passphrase used.
-  -r --rounds=<rounds>             The number of rounds used for PBKDF2i [default: 20000].
+  -r --rounds=<rounds>             The number of rounds used for PBKDF2 [default: 20000].
   -v --verbose                     Say more things.
      --version                     Show version.
 """
